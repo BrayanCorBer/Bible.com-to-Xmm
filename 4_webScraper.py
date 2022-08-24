65e6cb2f48024fa4daf426b7908f16e8ce82f4fc
brayancorber <brayancorber@gmail.com>
2022-08-24 17:16:29 -0500

    Get de Book name, Chapter number and logic for trought the entire bible and print full bible with respective tags in console

--- 4_webScraper.py
+++ 4_webScraper.py
@@ -10,6 +10,8 @@ from bs4 import BeautifulSoup
 import time
 import re
 
+nbla = ''
+
 options = webdriver.ChromeOptions()
 options.add_argument('--start-maximized')
 options.add_argument('--disable-extensions')
@@ -18,28 +20,94 @@ driver_path = 'C:\\Users\\braya\\Downloads\\chromedriver.exe'
 
 driver = webdriver.Chrome(driver_path, chrome_options=options)
 
-driver.get('https://www.bible.com/es/bible/103/HEB.9.NBLA')
+driver.get('https://www.bible.com/es/bible/103/GEN.1.NBLA')
 
 #Click in next arrow
 #WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.next-arrow'))).click()
 
-WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.chapter')))
-capitulo = driver.find_element(By.CSS_SELECTOR,'div.chapter')
-versiculos = capitulo.find_elements(By.CSS_SELECTOR,'span.verse')
-for verso in versiculos:
-    vv = verso.get_attribute('innerHTML')
-    soup = BeautifulSoup(vv, 'html.parser')
-    numerovv = soup.find_all('span',None)[0].text
-    if re.findall('[0-9]+', numerovv) != []:
-        print(numerovv)
-    versoArray = soup.select('span.content', None)
-    versoTxt=''
-    for v in versoArray:
-        versoTxt += v.text
-    print(versoTxt)
-    print('*'*5)
+# Recorrer toda la biblia
+nextArrow = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.next-arrow')))
 
-## Recorrer toda la biblia
+bookChecker = 'Génesis'
+while nextArrow:
+    ## Get Book Name
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.reader')))
+    bookContainer = driver.find_element(By.CSS_SELECTOR, 'div.reader')
+    bookTxt = bookContainer.get_attribute('innerHTML')
+    bookSoup = BeautifulSoup(bookTxt,'html.parser')
+    bookName = bookSoup.find_all('h1')[0].string
+    bookArray = bookName.split(' ')
+    bookArray.pop(-1)
+    book = ''
+    for b in bookArray:
+        book = book+b+' '
+    
+    if bookChecker != book:
+        print('</b>')
+        print('<b n="'+book[:-1]+'">')
+        bookChecker = book
+
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.chapter')))
+        capitulo = driver.find_element(By.CSS_SELECTOR,'div.chapter')
+
+        # Get Chap Number
+        capTxt = capitulo.get_attribute('innerHTML')
+        soup = BeautifulSoup(capTxt, 'html.parser')
+        capNum = soup.find_all('div')[0].string
+        print('<c n="'+capNum+'">')
+
+        # Get Verses with number
+
+        versiculos = capitulo.find_elements(By.CSS_SELECTOR,'span.verse')
+
+        for verso in versiculos:
+            vv = verso.get_attribute('innerHTML')
+            soup = BeautifulSoup(vv, 'html.parser')
+            numerovv = soup.find_all('span',None)[0].text
+            if re.findall('[0-9]+', numerovv) != []:
+                print('<v n="'+numerovv+'">')
+            versoArray = soup.select('span.content', None)
+            versoTxt=''
+            for v in versoArray:
+                versoTxt += v.text
+            print(versoTxt)
+            print('</v>')
+        print("</c>")
+
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.next-arrow'))).click()
+    else:
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.chapter')))
+        capitulo = driver.find_element(By.CSS_SELECTOR,'div.chapter')
+
+        # Get Chap Number
+        capTxt = capitulo.get_attribute('innerHTML')
+        soup = BeautifulSoup(capTxt, 'html.parser')
+        capNum = soup.find_all('div')[0].string
+        print('<c n="'+capNum+'">')
+
+        # Get Verses with number
+
+        versiculos = capitulo.find_elements(By.CSS_SELECTOR,'span.verse')
+
+        for verso in versiculos:
+            vv = verso.get_attribute('innerHTML')
+            soup = BeautifulSoup(vv, 'html.parser')
+            numerovv = soup.find_all('span',None)[0].text
+            if re.findall('[0-9]+', numerovv) != []:
+                print('<v n="'+numerovv+'">')
+            versoArray = soup.select('span.content', None)
+            versoTxt=''
+            for v in versoArray:
+                versoTxt += v.text
+            print(versoTxt)
+            print('</v>')
+        print("</c>")
+
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.next-arrow'))).click()
+
+
+
+# # Recorrer toda la biblia
 # nextArrow = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.next-arrow')))
 # while nextArrow:
 #   WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.next-arrow'))).click()
